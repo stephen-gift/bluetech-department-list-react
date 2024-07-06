2cd945b74a6a82c8dc1fcc5a0da5d0b6be4cae1f
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-06 01:54:26 +0100

    components

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+import BDLHeader from "./global/Header";
+import BDLSearchInput from "./global/Header/BDLSearchInput";
+import BDLMainLayout from "./global/Layout";
+import BDLTable from "./global/Table";
+// import BDLLoader from "./global/Loader";
+import BDLTableSkeleton from "./global/Loader/TableSkeleton";
+
+export { BDLHeader, BDLMainLayout, BDLSearchInput, BDLTable, BDLTableSkeleton };
