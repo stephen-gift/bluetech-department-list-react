49717c310199e84cca13db93c6b3f62b9cec37c9
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-06 01:58:36 +0100

    BDLLoader Update

--- src/components/global/Loader/index.tsx
+++ src/components/global/Loader/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-const index = (props: Props) => {
+const BDLLoader = (props: Props) => {
   return (
-    <div>index</div>
+    <div>BDLLoader</div>
   )
 }
 
-export default index
+export default BDLLoader
