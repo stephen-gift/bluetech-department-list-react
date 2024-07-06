f4622c143044d6127140df108878e80b55548a06
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-06 01:52:58 +0100

    Seo management

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BDLHomePageContainer } from "./containers";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <title>BlueTech Solutions - IT Services & Solutions</title>
+        <meta
+          name="description"
+          content="BlueTech Solutions LLC offers innovative IT services in web development, graphic design, network, and hardware solutions to enhance business efficiency."
+        />
+        <meta
+          name="keywords"
+          content="BlueTech Solutions, IT services, web development, graphic design, network solutions, hardware solutions, business solutions, technology provider, innovative technologies, customized solutions, business efficiency, pioneering solutions, long-term partnerships, future success"
+        />
+
+        <meta name="author" content="Blutech Solutions LLC" />
+        <link rel="author" href="https://bluetech-department-list.vercel.app" />
+
+        <meta property="og:title" content="Blutech Solutions LLC" />
+        <meta
+          property="og:description"
+          content="Blutech Solutions LLC is a technology company specializing in innovative software solutions. We provide cutting-edge services in web and mobile application development, cloud computing, and digital transformation strategies."
+        />
+        <meta property="og:site_name" content="Blutech Solutions LLC" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Blutech Solutions LLC" />
+        <meta
+          name="twitter:description"
+          content="Blutech Solutions LLC is a technology company specializing in innovative software solutions. We provide cutting-edge services in web and mobile application development, cloud computing, and digital transformation strategies."
+        />
+      </Helmet>
+      <BDLHomePageContainer />
+    </>
   );
 }
 
