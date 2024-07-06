c5aea2722f7a6fba0d5bba238ef71dfbee8e9b60
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-06 03:14:09 +0100

    Update seo to async

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,45 @@
 import "./App.css";
 import { BDLHomePageContainer } from "./containers";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <>
-      <Helmet>
-        <title>BlueTech Solutions - IT Services & Solutions</title>
-        <meta
-          name="description"
-          content="BlueTech Solutions LLC offers innovative IT services in web development, graphic design, network, and hardware solutions to enhance business efficiency."
-        />
-        <meta
-          name="keywords"
-          content="BlueTech Solutions, IT services, web development, graphic design, network solutions, hardware solutions, business solutions, technology provider, innovative technologies, customized solutions, business efficiency, pioneering solutions, long-term partnerships, future success"
-        />
+    <HelmetProvider>
+      <>
+        <Helmet>
+          <title>BlueTech Solutions - IT Services & Solutions</title>
+          <meta
+            name="description"
+            content="BlueTech Solutions LLC offers innovative IT services in web development, graphic design, network, and hardware solutions to enhance business efficiency."
+          />
+          <meta
+            name="keywords"
+            content="BlueTech Solutions, IT services, web development, graphic design, network solutions, hardware solutions, business solutions, technology provider, innovative technologies, customized solutions, business efficiency, pioneering solutions, long-term partnerships, future success"
+          />
 
-        <meta name="author" content="Blutech Solutions LLC" />
-        <link rel="author" href="https://bluetech-department-list.vercel.app" />
+          <meta name="author" content="Blutech Solutions LLC" />
+          <link
+            rel="author"
+            href="https://bluetech-department-list.vercel.app"
+          />
 
-        <meta property="og:title" content="Blutech Solutions LLC" />
-        <meta
-          property="og:description"
-          content="Blutech Solutions LLC is a technology company specializing in innovative software solutions. We provide cutting-edge services in web and mobile application development, cloud computing, and digital transformation strategies."
-        />
-        <meta property="og:site_name" content="Blutech Solutions LLC" />
+          <meta property="og:title" content="Blutech Solutions LLC" />
+          <meta
+            property="og:description"
+            content="Blutech Solutions LLC is a technology company specializing in innovative software solutions. We provide cutting-edge services in web and mobile application development, cloud computing, and digital transformation strategies."
+          />
+          <meta property="og:site_name" content="Blutech Solutions LLC" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Blutech Solutions LLC" />
-        <meta
-          name="twitter:description"
-          content="Blutech Solutions LLC is a technology company specializing in innovative software solutions. We provide cutting-edge services in web and mobile application development, cloud computing, and digital transformation strategies."
-        />
-      </Helmet>
-      <BDLHomePageContainer />
-    </>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Blutech Solutions LLC" />
+          <meta
+            name="twitter:description"
+            content="Blutech Solutions LLC is a technology company specializing in innovative software solutions. We provide cutting-edge services in web and mobile application development, cloud computing, and digital transformation strategies."
+          />
+        </Helmet>
+        <BDLHomePageContainer />
+      </>
+    </HelmetProvider>
   );
 }
 
